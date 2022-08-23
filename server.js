990b6600d2071f39c7f22fb46c357867485b162b
Golam Mostafa <mostafarmstu@gmail.com>
2022-08-24 03:35:20 +0600

    update

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/assignment'));
 
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+app.use(requireHTTPS);
+
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname + '/dist/assignment/index.html'));
 })
