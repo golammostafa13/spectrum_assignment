a44abc1aee5f7652bfd001d2d092d33230dc2001
Golam Mostafa <mostafarmstu@gmail.com>
2022-08-24 02:30:11 +0600

    upload

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm !: FormGroup;
+  imgUrl : string = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLvPJKXmD3mIlfOVee-apUyIhjnkCDFLtLGpxUA5-8hA&s";
+
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: [''],
+      password: [''],
+    })
+  }
+  loginHandler(){
+    this.http.get<any>("http://localhost:3000/signupUsers")
+    .subscribe(res => {
+      const user = res.find((u:any) => {
+        return u.email === this.loginForm.value.email && u.password === this.loginForm.value.password
+      });
+      if(user){
+        alert("Login Success!!");
+        this.loginForm.reset();
+        this.router.navigate(['dashboard'])
+      }else{
+        alert("User not Found!!!");
+        // this.router.navigate
+      }
+    })
+  }
+}
