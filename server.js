0ed5ffd260bc3212c87f2097cd4276d98cbf788c
Golam Mostafa <mostafarmstu@gmail.com>
2022-08-24 05:25:14 +0600

    update

--- server.js
+++ server.js
@@ -4,24 +4,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/assignment/src'));
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    next();
-  })
-app.use(requireHTTPS);
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
+app.use(express.static('./dist/assignment'));
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/assignment/src/index.html'));
-})
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/assignment/'}),
+);
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
