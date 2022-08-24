b7cbdd1fd82ee07ce1235d4bdfb305b9b6fb56b1
Golam Mostafa <mostafarmstu@gmail.com>
2022-08-24 17:00:10 +0600

    update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8000;
 app.use('/api', jsonServer.router('db.json'));
 app.use(express.static(__dirname+'/dist/assignment'));
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(__dirname+'/dist/assignment/index.html');
 });
 
