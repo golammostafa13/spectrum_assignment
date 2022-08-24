2f1558fef7e2820af5c34df38d135ebbae435611
Golam Mostafa <mostafarmstu@gmail.com>
2022-08-25 02:10:42 +0600

    update

--- server.js
+++ server.js
@@ -1,17 +1,40 @@
-const express = require('express');
+// const express = require('express');
+// const jsonServer = require('json-server');
+
+// const app = express();
+
+// const PORT = process.env.PORT || 8000;
+
+// app.use('http://localhost:3000', jsonServer.router('db.json'));
+// app.use(express.static(__dirname+'/dist/assignment'));
+
+// app.get('/*', (req, res) => {
+//     res.sendFile(__dirname+'/dist/assignment/index.html');
+// });
+
+// app.listen(PORT, () => {
+//     console.log('app is listening at '+PORT);
+// })
+
 const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./db.json');
 
-const app = express();
-
-const PORT = process.env.PORT || 8000;
-
-app.use('http://localhost:3000', jsonServer.router('db.json'));
-app.use(express.static(__dirname+'/dist/assignment'));
-
-app.get('/*', (req, res) => {
-    res.sendFile(__dirname+'/dist/assignment/index.html');
+const middlewares = jsonServer.defaults({
+  static: './dist/assignment',
 });
 
-app.listen(PORT, () => {
-    console.log('app is listening at '+PORT);
-})
+const PORT = process.env.PORT || 8000;
+server.use(middlewares);
+
+server.use(
+  jsonServer.rewriter({
+    '/api/*': '/$1',
+  })
+);
+
+server.use(router);
+server.listen(PORT, () => {
+  console.log('Server is running');
+});
+
