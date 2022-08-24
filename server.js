bcffbc2ccb23a1d887c92bd5c16c508fb9487743
Golam Mostafa <mostafarmstu@gmail.com>
2022-08-25 02:33:55 +0600

    update

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const middlewares = jsonServer.defaults({
 });
 
 server.get('/*', (req, res) => {
-    res.sendFile('./dist/assignment/index.html');
+    res.sendFile(__dirname+'/dist/assignment/index.html');
 });
 
 const PORT = process.env.PORT || 8000;
