e9a031984bfff423282f5f511d53db7081377375
Golam Mostafa <mostafarmstu@gmail.com>
2022-08-24 12:47:17 +0600

    update

--- server.js
+++ server.js
@@ -4,21 +4,21 @@ const path = require('path');
 
 const app = express();
 
-const distDir = __dirname + '/dist'
+const distDir = __dirname + '/dist/'
 
 app.use(express.static(distDir));
-app.use((req, res, next) => {
+// app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
+//     res.setHeader(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//     );
     res.setHeader(
         "Access-Control-Allow-Methods",
         "GET, POST, PATCH, PUT, DELETE, OPTIONS"
     );
     next();
-});
+// });
 
 app.get('/*', (req, res) =>
     res.sendFile(path.join(__dirname + '/dist/index.html'))
